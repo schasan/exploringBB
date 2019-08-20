a20caf742e44c4d2db78162a6cada2452fb6d91b
Demo User <debian@beaglebone.localdomain>
2019-08-20 06:38:13 +0000

    Strip all output to LED

--- extras/kernel/gpio_test/gpio_test.c
+++ extras/kernel/gpio_test/gpio_test.c
@@ -20,11 +20,9 @@ MODULE_AUTHOR("Derek Molloy");
 MODULE_DESCRIPTION("A Button/LED test driver for the BBB");
 MODULE_VERSION("0.1");
 
-static unsigned int gpioLED = 49;       ///< hard coding the LED gpio for this example to P9_23 (GPIO49)
-static unsigned int gpioButton = 115;   ///< hard coding the button gpio for this example to P9_27 (GPIO115)
+static unsigned int gpioButton = 60;    ///< hard coding the button gpio for this example to P9_27 (GPIO115)
 static unsigned int irqNumber;          ///< Used to share the IRQ number within this file
 static unsigned int numberPresses = 0;  ///< For information, store the number of button presses
-static bool	    ledOn = 0;          ///< Is the LED on or off? Used to invert its state (off by default)
 
 /// Function prototype for the custom IRQ handler function -- see below for the implementation
 static irq_handler_t  ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
@@ -40,20 +38,9 @@ static int __init ebbgpio_init(void){
    int result = 0;
    printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
    // Is the GPIO a valid GPIO number (e.g., the BBB has 4x32 but not all available)
-   if (!gpio_is_valid(gpioLED)){
-      printk(KERN_INFO "GPIO_TEST: invalid LED GPIO\n");
-      return -ENODEV;
-   }
-   // Going to set up the LED. It is a GPIO in output mode and will be on by default
-   ledOn = true;
-   gpio_request(gpioLED, "sysfs");          // gpioLED is hardcoded to 49, request it
-   gpio_direction_output(gpioLED, ledOn);   // Set the gpio to be in output mode and on
-// gpio_set_value(gpioLED, ledOn);          // Not required as set by line above (here for reference)
-   gpio_export(gpioLED, false);             // Causes gpio49 to appear in /sys/class/gpio
-			                    // the bool argument prevents the direction from being changed
    gpio_request(gpioButton, "sysfs");       // Set up the gpioButton
    gpio_direction_input(gpioButton);        // Set the button GPIO to be an input
-   gpio_set_debounce(gpioButton, 200);      // Debounce the button with a delay of 200ms
+   //gpio_set_debounce(gpioButton, 200);      // Debounce the button with a delay of 200ms
    gpio_export(gpioButton, false);          // Causes gpio115 to appear in /sys/class/gpio
 			                    // the bool argument prevents the direction from being changed
    // Perform a quick test to see that the button is working as expected on LKM load
@@ -82,11 +69,8 @@ static int __init ebbgpio_init(void){
 static void __exit ebbgpio_exit(void){
    printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioButton));
    printk(KERN_INFO "GPIO_TEST: The button was pressed %d times\n", numberPresses);
-   gpio_set_value(gpioLED, 0);              // Turn the LED off, makes it clear the device was unloaded
-   gpio_unexport(gpioLED);                  // Unexport the LED GPIO
    free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
    gpio_unexport(gpioButton);               // Unexport the Button GPIO
-   gpio_free(gpioLED);                      // Free the LED GPIO
    gpio_free(gpioButton);                   // Free the Button GPIO
    printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
 }
@@ -102,8 +86,6 @@ static void __exit ebbgpio_exit(void){
  *  return returns IRQ_HANDLED if successful -- should return IRQ_NONE otherwise.
  */
 static irq_handler_t ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
-   ledOn = !ledOn;                          // Invert the LED state on each button press
-   gpio_set_value(gpioLED, ledOn);          // Set the physical LED accordingly
    printk(KERN_INFO "GPIO_TEST: Interrupt! (button state is %d)\n", gpio_get_value(gpioButton));
    numberPresses++;                         // Global counter, will be outputted when the module is unloaded
    return (irq_handler_t) IRQ_HANDLED;      // Announce that the IRQ has been handled correctly
