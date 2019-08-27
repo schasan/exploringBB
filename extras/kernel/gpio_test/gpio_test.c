4900d3f13977b364edfc26fa4536998543a1ba7a
Mario Schulz <Mario.Schulz@arcor.de>
2019-08-27 10:56:58 +0000

    Merge reader device code

--- extras/kernel/gpio_test/gpio_test.c
+++ extras/kernel/gpio_test/gpio_test.c
@@ -9,20 +9,33 @@
  * @see http://www.derekmolloy.ie/
 */
 
+#include "gpio_test.h"
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/gpio.h>                 // Required for the GPIO functions
 #include <linux/interrupt.h>            // Required for the IRQ code
+#include <linux/timekeeping.h>		// Measure time between interrupts
+#include <linux/uaccess.h>		// copy_to_user()
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Derek Molloy");
 MODULE_DESCRIPTION("A Button/LED test driver for the BBB");
 MODULE_VERSION("0.1");
 
-static unsigned int gpioButton = 60;    ///< hard coding the button gpio for this example to P9_27 (GPIO115)
+static unsigned int gpioPulse = 60;    ///< hard coding the button gpio for this example to P9_27 (GPIO115)
 static unsigned int irqNumber;          ///< Used to share the IRQ number within this file
-static unsigned int numberPresses = 0;  ///< For information, store the number of button presses
+static unsigned int numberPulses = 0;  ///< For information, store the number of button presses
+static ktime_t interrupt_time = 0;
+static int ring_read_ptr = 0;
+static int ring_write_ptr = 0;
+#define RING_SIZE 4096
+
+struct {
+	int pulse_number;
+	ktime_t interrupt_time;
+	ktime_t interrupt_delta;
+} ring[RING_SIZE];
 
 /// Function prototype for the custom IRQ handler function -- see below for the implementation
 static irq_handler_t  ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs);
@@ -38,16 +51,16 @@ static int __init ebbgpio_init(void){
    int result = 0;
    printk(KERN_INFO "GPIO_TEST: Initializing the GPIO_TEST LKM\n");
    // Is the GPIO a valid GPIO number (e.g., the BBB has 4x32 but not all available)
-   gpio_request(gpioButton, "sysfs");       // Set up the gpioButton
-   gpio_direction_input(gpioButton);        // Set the button GPIO to be an input
-   //gpio_set_debounce(gpioButton, 200);      // Debounce the button with a delay of 200ms
-   gpio_export(gpioButton, false);          // Causes gpio115 to appear in /sys/class/gpio
+   gpio_request(gpioPulse, "sysfs");       // Set up the gpioPulse
+   gpio_direction_input(gpioPulse);        // Set the button GPIO to be an input
+   //gpio_set_debounce(gpioPulse, 200);      // Debounce the button with a delay of 200ms
+   gpio_export(gpioPulse, false);          // Causes gpio115 to appear in /sys/class/gpio
 			                    // the bool argument prevents the direction from being changed
    // Perform a quick test to see that the button is working as expected on LKM load
-   printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioButton));
+   printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioPulse));
 
    // GPIO numbers and IRQ numbers are not the same! This function performs the mapping for us
-   irqNumber = gpio_to_irq(gpioButton);
+   irqNumber = gpio_to_irq(gpioPulse);
    printk(KERN_INFO "GPIO_TEST: The button is mapped to IRQ: %d\n", irqNumber);
 
    // This next call requests an interrupt line
@@ -58,6 +71,9 @@ static int __init ebbgpio_init(void){
                         NULL);                 // The *dev_id for shared interrupt lines, NULL is okay
 
    printk(KERN_INFO "GPIO_TEST: The interrupt request result is: %d\n", result);
+   
+   result = register_device();
+   printk(KERN_INFO "GPIO_TEST: The device register request result is: %d\n", result);
    return result;
 }
 
@@ -67,11 +83,11 @@ static int __init ebbgpio_init(void){
  *  GPIOs and display cleanup messages.
  */
 static void __exit ebbgpio_exit(void){
-   printk(KERN_INFO "GPIO_TEST: The button state is currently: %d\n", gpio_get_value(gpioButton));
-   printk(KERN_INFO "GPIO_TEST: The button was pressed %d times\n", numberPresses);
+   printk(KERN_INFO "GPIO_TEST: Interrupts received: %d\n", numberPulses);
    free_irq(irqNumber, NULL);               // Free the IRQ number, no *dev_id required in this case
-   gpio_unexport(gpioButton);               // Unexport the Button GPIO
-   gpio_free(gpioButton);                   // Free the Button GPIO
+   gpio_unexport(gpioPulse);               // Unexport the Button GPIO
+   gpio_free(gpioPulse);                   // Free the Button GPIO
+   unregister_device();
    printk(KERN_INFO "GPIO_TEST: Goodbye from the LKM!\n");
 }
 
@@ -86,11 +102,77 @@ static void __exit ebbgpio_exit(void){
  *  return returns IRQ_HANDLED if successful -- should return IRQ_NONE otherwise.
  */
 static irq_handler_t ebbgpio_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
-   printk(KERN_INFO "GPIO_TEST: Interrupt! (button state is %d)\n", gpio_get_value(gpioButton));
-   numberPresses++;                         // Global counter, will be outputted when the module is unloaded
+   ktime_t now = ktime_get();
+
+   //printk(KERN_INFO "GPIO_TEST: Interrupt! (button state is %d)\n", gpio_get_value(gpioPulse));
+   if (numberPulses)
+      printk(KERN_INFO "GPIO_TEST: Interrupt! Pulse number %08d %12lld\n", numberPulses, now-interrupt_time);
+   else
+      printk(KERN_INFO "GPIO_TEST: Interrupt! Pulse number %08d no delta\n", numberPulses);
+   
+   ring[ring_write_ptr].pulse_number = numberPulses++;    // Global counter, will be outputted when the module is unloaded
+   ring[ring_write_ptr].interrupt_delta = now-interrupt_time;
+   ring[ring_write_ptr].interrupt_time = now;
+   ring_write_ptr++;
+   ring_write_ptr %= RING_SIZE;
+
+   interrupt_time = now;
+
    return (irq_handler_t) IRQ_HANDLED;      // Announce that the IRQ has been handled correctly
 }
 
+static const char    g_s_Hello_World_string[] = "Hello world from kernel mode!\n\0";
+static const ssize_t g_s_Hello_World_size = sizeof(g_s_Hello_World_string);
+
+/*===============================================================================================*/
+static ssize_t device_file_read(struct file *file_ptr, char __user *user_buffer, size_t count, loff_t *possition)
+{
+   printk( KERN_NOTICE "Simple-driver: Device file is read at offset = %i, read bytes count = %u", (int)*possition, (unsigned int)count );
+
+   if (*possition >= g_s_Hello_World_size) return 0;
+   if (*possition + count > g_s_Hello_World_size) count = g_s_Hello_World_size - *possition;
+   if (copy_to_user(user_buffer, g_s_Hello_World_string + *possition, count) != 0) return -EFAULT;
+
+   *possition += count;
+   return count;
+}
+
+/*===============================================================================================*/
+static struct file_operations simple_driver_fops =
+{
+   .owner   = THIS_MODULE,
+   .read    = device_file_read,
+};
+
+static int device_file_major_number = 0;
+static const char device_name[] = "Simple-driver";
+
+/*===============================================================================================*/
+int register_device(void)
+{
+   int result = 0;
+
+   printk( KERN_NOTICE "Simple-driver: register_device() is called." );
+
+   result = register_chrdev(0, device_name, &simple_driver_fops);
+   if (result < 0) {
+     printk(KERN_WARNING "Simple-driver:  can\'t register character device with errorcode = %i", result);
+     return result;
+   }
+
+   device_file_major_number = result;
+   printk(KERN_NOTICE "Simple-driver: registered character device with major number = %i and minor numbers 0...255", device_file_major_number);
+
+   return 0;
+}
+
+/*-----------------------------------------------------------------------------------------------*/
+void unregister_device(void)
+{
+   printk(KERN_NOTICE "Simple-driver: unregister_device() is called");
+   if (device_file_major_number != 0) unregister_chrdev(device_file_major_number, device_name);
+}
+//
 /// This next calls are  mandatory -- they identify the initialization function
 /// and the cleanup function (as above).
 module_init(ebbgpio_init);
